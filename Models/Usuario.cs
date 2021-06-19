7a205bc827769c29e960d61f1fb0dac48feda7e2
armando555 <armandosebas4@gmail.com>
2021-06-18 23:00:23 -0500

    Agregar archivos de proyecto.

---
+++ Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudEntityFramework.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        [Display(Name ="Teléfono")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "El celular es obligatorio")]
+        [Display(Name = "Celular")]
+        public string celular{ get; set; }
+        [Required(ErrorMessage = "El e-mail es obligatorio")]
+        public string Email { get; set; }
+
+    }
+}
